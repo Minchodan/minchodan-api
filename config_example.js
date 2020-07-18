997dbf7e51e7e5b5563d66ebde4a3372f2593199
lio8192 <km8192@naver.com>
2020-07-19 03:08:15 +0900

    DB설정 예시파일

---
+++ config_example.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': 'MiNChoDANisGrEAtFuL',
+    'mongodbUri': 'mongodb://minchodan:<Password>@kyongmin-shard-00-00.1op9i.gcp.mongodb.net:27017,kyongmin-shard-00-01.1op9i.gcp.mongodb.net:27017,kyongmin-shard-00-02.1op9i.gcp.mongodb.net:27017/minchodan?ssl=true&replicaSet=atlas-kavkqc-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
